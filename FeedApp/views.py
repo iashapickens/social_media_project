82106eeb2162c653042f800d0cd5e4ca4c8510b2
Iasha Pickens <iasha_pickens1@baylor.edu>
2025-10-11 18:22:32 -0500

    Added My Profile

--- FeedApp/views.py
+++ FeedApp/views.py
@@ -18,8 +18,23 @@ def index(request):
 
 
 
-#@login_required
+@login_required
+def profile(request):
+    profile = Profile.objects.filter(user=request.user)
+    if not profile.exists():
+        Profile.objects.create(user=request.user)
+    profile = Profile.objects.get(user=request.user)
 
+    if request.method != 'POST':
+        form = ProfileForm(instance=profile)
+    else:
+        form = ProfileForm(instance=profile,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('FeedApp:profile')
+        
+    context = {'form': form}
+    return render(request, 'FeedApp/profile.html', context)
 
 
 
